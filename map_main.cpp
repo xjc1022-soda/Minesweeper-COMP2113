7b57c5138c96638f1facce0c4d76c838f9e192a6
h3568933 <78430167+h3568933@users.noreply.github.com>
2021-04-05 17:00:36 +0800

    Create map_main.cpp

--- map_main.cpp
+++ map_main.cpp
@@ -10,6 +10,7 @@ using namesapce std;
 struct Position {
    int x;
    int y;};
+
 int main(){
    showinstruction(instruction.txt);
    srand(time(NULL));
@@ -30,7 +31,76 @@ int main(){
    
    int initial_pos=rand()%10;
    Position start={initial_pos, 0};
+   Position current = start;
    
-   printmap(map, heal_pack, currentpos);
+   printmap(map, heal_pack, current);
+   
+   int out=rand()%10;
+   Position end={out,9};
+   
+   int blood=3;
+   
+   
+   char input;
+   while (true){
+      cout<<"Your current position is: "<<current.x<<" "<<current.y<<endl;
+      cout<<"Number of landmines around you: "<<map[current.x][current.y]<<endl;
+      cout << "You have " << blood << " blood(s)." <<endl;
+      cout<<"Please input your next action: ";
+      cin>>input;
+      
+      if (input == 'w'){
+        if (current.x == 0){
+          cout << "Invalid movement!" << endl;
+          continue;}
+        else if ( map[current.x-1][current.y] == 999 ){
+          cout << "This is a wall, you cannot pass through it." << endl;
+          countine;}
+        else 
+          current.x -= 1; 
+      }
+      else if (input == 'a'){
+        if (current.y == 0){
+          cout << "Invalid movement!" << endl;
+          continue;}
+        else if ( map[current.x][current.y-1] == 999 ){
+          cout << "This is a wall, you cannot pass through it." << endl;
+          countine;}
+        else 
+          current.y -= 1; 
+      }
+      if (input == 's'){
+        if (current.x == 9){
+          cout << "Invalid movement!" << endl;
+          continue;}
+        else if ( map[current.x+1][current.y] == 999 ){
+          cout << "This is a wall, you cannot pass through it." << endl;
+          countine;} 
+        else 
+          current.x += 1; 
+      }
+      else if (input == 'd'){
+        if (current.y == 9){
+          cout << "Invalid movement!" << endl;
+          continue;}
+        else if ( map[current.x][current.y+1] == 999 ){
+          cout << "This is a wall, you cannot pass through it." << endl;
+          countine;}
+        else 
+          current.y += 1; 
+      }
+      
+      if ( map[current.x][current.y] == 100 ){
+         cout << "You reach a landmine!" << endl;
+         blood-=1;
+         if (blood == 0){
+            cout << "You use up all your blood" << endl << "You lose!" <<endl;
+            break;}
+         else if (blood > 0){
+            cout << "Go back and try again!" << endl;
+            int restart_pos=rand()%10;
+            Position current={restart_pos, 0};
+            continue;}
+        }
    
 }
