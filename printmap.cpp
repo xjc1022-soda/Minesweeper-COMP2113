95ccccf4c26905ec377c030a04e0899dd3f8a08a
xjc1022-soda <80735003+xjc1022-soda@users.noreply.github.com>
2021-04-05 16:55:55 +0800

    Add files via upload

---
+++ printmap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "printmap.h"
+using namespace std;
+
+struct Position {
+   int x;
+   int y;};
+void printmap(int map[][10];Position heal_pack, Position currentpos){
+   for(int i=0; i<10; i++){
+    for(int j=0; j<10; j++){
+        if ({i,j}==heal_pack)
+            cout<<"*"<<" ";
+        else if({i,j}==currentpos)
+            cout<<"O"<<" ";
+        else
+            cout<<"x"<<" ";
+    }
+     cout<<endl;
+   }
+}
---
+++ printmap.h
@@ -0,0 +1,4 @@
+#ifndef PRINTMAP_H
+#define PRINTMAP_H
+void printmap(map[][10], Position heal_pack, Postion currentpos)
+#endif
