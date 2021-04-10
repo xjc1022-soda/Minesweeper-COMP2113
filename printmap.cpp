940ba3dd85686b0d21a098bae2d92a68d8418f1e
h3568933 <78430167+h3568933@users.noreply.github.com>
2021-04-10 16:25:30 +0800

    Update printmap.cpp

--- printmap.cpp
+++ printmap.cpp
@@ -2,15 +2,13 @@
 #include "printmap.h"
 using namespace std;
 
-struct Position {
-   int x;
-   int y;};
-void printmap(int map[][10];Position heal_pack, Position current){
+
+void printmap(int map[][10], Position heal_pack, Position current){
    for(int i=0; i<10; i++){
     for(int j=0; j<10; j++){
-        if ({i,j}==heal_pack)
+        if (i==heal_pack.x && heal_pack.y==j)
             cout<<"*"<<" ";
-        else if({i,j}==currentpos)
+        else if(i==current.x && j==current.y)
             cout<<"O"<<" ";
         else
             cout<<"x"<<" ";
