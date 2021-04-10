49d489f91636c39381d60170e8eed927bac607c2
h3568933 <78430167+h3568933@users.noreply.github.com>
2021-04-10 16:17:38 +0800

    Update map_main.cpp

--- map_main.cpp
+++ map_main.cpp
@@ -5,49 +5,51 @@
 #include<ctime>
 #include "printmap.h"
 
-using namesapce std;
+using namespace std;
 
-struct Position {
-   int x;
-   int y;
-};
+
 
 int main(){
-   showinstruction(instruction.txt);
+   showinstruction();
    srand(time(NULL));
    int number=rand()%5+1;
    int map[10][10];
    mapContainer(map, number);
-   
+   Position heal_pack;
    if (number == 1){
-     Position heal_pack={1,7};
+     heal_pack.x=1;
+     heal_pack.y=7;
    }
    else if (number == 2){
-     Postion heal_pack={3,5};
+     heal_pack.x=3;
+     heal_pack.y=5;
    }
    else if (number == 3){
-     Postion heal_pack={6,5};
+     heal_pack.x=6;
+     heal_pack.y=5;
    }
    else if (number == 4){
-     Postion heal_pack={5,4};
-   }   
+     heal_pack.x=5;
+     heal_pack.y=4;
+   }
    else if (number == 5){
-     Postion heal_pack={5,4};
+     heal_pack.x=5;
+     heal_pack.y=4;
    }
    int number_of_heal_pack = 1;
-   
+
    int initial_pos=rand()%10;
    Position start={initial_pos, 0};
    Position current = start;
-   
+
    printmap(map, heal_pack, current);
-   
+
    int out=rand()%10;
    Position end={out,9};
-   
+
    int blood=3;
-   
-   
+
+
    char input;
    while (true){
       cout<<"Your current position is: "<<current.x<<" "<<current.y<<endl;
@@ -55,48 +57,56 @@ int main(){
       cout << "You have " << blood << " blood(s)." <<endl;
       cout<<"Please input your next action: ";
       cin>>input;
-      
+
       if (input == 'w'){
         if (current.x == 0){
-          cout << "Invalid movement!" << endl;
+          cout << "You reach the boundary, invalid movement!" << endl;
+          printmap(map, heal_pack, current);
           continue;}
         else if ( map[current.x-1][current.y] == 999 ){
           cout << "This is a wall, you cannot pass through it." << endl;
-          countine;}
-        else 
-          current.x -= 1; 
+          printmap(map, heal_pack, current);
+          continue;}
+        else
+          current.x -= 1;
       }
       else if (input == 'a'){
         if (current.y == 0){
-          cout << "Invalid movement!" << endl;
+          cout << "You reach the boundary, invalid movement!" << endl;
+          printmap(map, heal_pack, current);
           continue;}
         else if ( map[current.x][current.y-1] == 999 ){
           cout << "This is a wall, you cannot pass through it." << endl;
-          countine;}
-        else 
-          current.y -= 1; 
+          printmap(map, heal_pack, current);
+          continue;}
+        else
+          current.y -= 1;
       }
       else if (input == 's'){
         if (current.x == 9){
-          cout << "Invalid movement!" << endl;
+          cout << "You reach the boundary, invalid movement!" << endl;
+          printmap(map, heal_pack, current);
           continue;}
         else if ( map[current.x+1][current.y] == 999 ){
           cout << "This is a wall, you cannot pass through it." << endl;
-          countine;} 
-        else 
-          current.x += 1; 
+          printmap(map, heal_pack, current);
+          continue;}
+        else
+          current.x += 1;
       }
       else if (input == 'd'){
         if (current.y == 9){
-          cout << "Invalid movement!" << endl;
+          cout << "You reach the boundary, invalid movement!" << endl;
+          printmap(map, heal_pack, current);
           continue;}
         else if ( map[current.x][current.y+1] == 999 ){
           cout << "This is a wall, you cannot pass through it." << endl;
-          countine;}
-        else 
-          current.y += 1; 
+          printmap(map, heal_pack, current);
+          continue;}
+        else
+          current.y += 1;
       }
-      
+
       if ( map[current.x][current.y] == 100 ){
          cout << "You reach a landmine!" << endl;
          blood-=1;
@@ -106,10 +116,12 @@ int main(){
          else if (blood > 0){
             cout << "Go back and try again!" << endl;
             int restart_pos=rand()%10;
-            Position current={restart_pos, 0};
+            current.x = restart_pos;
+            current.y = 0;
+            printmap(map, heal_pack, current);
             continue;}
         }
-      
+
       else if ( current.x == end.x && current.y == 9){
          cout << "Congratulation, you reach the endpoint!" << endl;
          cout << "You win!" <<endl;
@@ -117,12 +129,17 @@ int main(){
       }
       else if ( current.x == heal_pack.x && current.y == heal_pack.y){
          if (number_of_heal_pack == 0){
-            cout << "You have already picked up the heal pack." << endl;}
+            cout << "You have already picked up the heal pack. You cannot pick the heal pack any more!" << endl;
+            printmap(map, heal_pack, current);
+          }
          else {
             blood += 1;
             cout << "You get a heal pack. One blood added!" << endl;
-            number_of_heal_pack -= 1; }
-      else{ 
+            number_of_heal_pack -= 1;
+            printmap(map, heal_pack, current);
+           }
+         }
+      else{
          printmap(map, heal_pack, current);
          continue;
       }
