07a435098a75308f2e449cae2a07a918aa0b78eb
xjc1022-soda <80735003+xjc1022-soda@users.noreply.github.com>
2021-04-10 20:20:17 +0800

    Update instruction.cpp

--- instruction.cpp
+++ instruction.cpp
@@ -4,16 +4,19 @@
 #include<fstream>
 using namespace std;
 
+// read instruction from "instruction.txt"  and print it out to the player 
 void showinstruction(){
    ifstream fin;
    fin.open("instruction.txt");
    if ( fin.fail() ) {
-      cout << "Error in file opening!" << endl;
+      cout << "Error in file opening!" << endl;    
+      //test whether can read from the text
       exit(1);
    }
    string x;
    while(getline(fin,x)){
-    cout<<x<<endl;
+    cout<<x<<endl;                     
+      //print out the instruction
    }
    fin.close();
 }
