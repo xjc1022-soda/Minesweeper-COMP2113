bde99a2919e2cabe3010b2b29d6c0a6bea4e65cb
xjc1022-soda <80735003+xjc1022-soda@users.noreply.github.com>
2021-04-10 20:46:21 +0800

    Update printmap.cpp

--- printmap.cpp
+++ printmap.cpp
@@ -2,7 +2,8 @@
 #include "printmap.h"
 using namespace std;
 
-
+// to visialize the map for the player 
+// show exsiting position and heal pack for the player
 void printmap(int map[][10], Position heal_pack, Position current){
    for(int i=0; i<10; i++){
     for(int j=0; j<10; j++){
