e97c58d49b1e95d88b0aefb5abdfb2d0ea9a2285
h3568933 <78430167+h3568933@users.noreply.github.com>
2021-04-10 16:23:41 +0800

    Update printmap.h

--- printmap.h
+++ printmap.h
@@ -1,7 +1,7 @@
 #ifndef PRINTMAP_H
 #define PRINTMAP_H
-struct Position {
-   int x;
-   int y;};
-void printmap(map[][10], Position heal_pack, Position current);
+struct Position{
+  int x;
+  int y;};
+void printmap(int map[][10], Position heal_pack, Position current);
 #endif
