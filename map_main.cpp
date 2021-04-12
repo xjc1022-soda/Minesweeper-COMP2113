b067798d08f89f7a975bf4e070dfa42a57c1458b
xjc1022-soda <80735003+xjc1022-soda@users.noreply.github.com>
2021-04-12 13:21:25 +0800

    Update map_main.cpp

--- map_main.cpp
+++ map_main.cpp
@@ -10,52 +10,59 @@ using namespace std;
 
 
 int main(){
-   // before the game begin, provide players with basic instructions and rules about the game
+
+// before the game begin, provide players with basic instructions and rules about the game
    showinstruction();
-   // use current time to genrate a random number from 1-5 for map genration
+   
+// use current time to genrate a random number from 1-5 for map genration
    srand(time(NULL));
    int number=rand()%5+1;
    int map[10][10];
-   // genrate a map 
+
+// genrate a map 
    mapContainer(map, number);
-   // define the position of heal_pack
+   
+// define the position of heal_pack
    Position heal_pack;
    heal_pack.x=5;
    heal_pack.y=4;
    int number_of_heal_pack = 1;
    
-   // initialize the beginning position on the first colomn
+// initialize the beginning position on the first colomn
    int initial_pos=rand()%10;
    Position start={initial_pos, 0};
    Position current = start;
 
    
-   // setting the end position of the game
+// setting the end position of the game
    int out = rand()%10;
    Position end = {out,9};
-   // use dynamic variable to set the blood of player
+   
+// use dynamic variable to set the blood of player
    int * blood = new int(3);
 
    
    char input;
    int expect_num;
-   // use dynamic variable to store the steps
+   
+// use dynamic variable to store the steps
    int * num_step = new int(0);
    
-   // initialize marks which will be use to rate players performance in the game
+// initialize marks which will be use to rate players performance in the game
    int * mark = new int(0);
    cout<<"Input the expected number of movement you need to reach end point: ";
    
-   //require player to input an int to estimate steps he need to finish the game
+//require player to input an int to estimate steps he need to finish the game
    cin>>expect_num;
    int * num_of_tool = new int (1);
    
-   // print out the map to help player carry on his game
-   // 'o' represent current position '*' represent heal_pack
+// print out the map to help player carry on his game
+// 'o' represent current position '*' represent heal_pack
    printmap(map, heal_pack, current);
    
    while (true){
-      // provide information and summary of current condition after each step
+      
+  // provide information and summary of current condition after each step
       cout<<"Your current position is: "<<current.x<<" "<<current.y<<endl;
       cout<<"Number of landmines around you: "<<map[current.x][current.y]<<endl;
       cout << "You have " << *blood << " blood(s)." <<endl;
@@ -63,9 +70,10 @@ int main(){
       cin>>input;
       *num_step+=1;
       
-      // define the functin of special tool
+  // define the functin of special tool
       if (input == 'p') {
-         // players can use special tool at most one time
+         
+  // players can use special tool at most one time
          if ( *num_of_tool == 0 ) {
             cout << "You have used up all the tools, you cannot use it any more!" <<endl;
             *num_step -= 1;
@@ -74,24 +82,28 @@ int main(){
          else {
             cout << "You are now using special tool, please input your movement: ";
             cin >> input;
-            // classify situaions into four direction movement
+            
+   // classify situaions into four direction movement
             if (input == 'w'){
                // the situation of invalid movement
                if (current.x == 0 || current.x == 1){
                   cout << "You reach the boundary, invalid movement!" << endl;
                   printmap(map, heal_pack, current);
                   continue;}
-               // the situation of running into a wall
+               
+  // the situation of running into a wall
                else if ( map[current.x-2][current.y] == 999 ){
                   cout << "This is a wall, you cannot reach it." << endl;
                   printmap(map, heal_pack, current);
                   continue;}
-               // situation that tool is used successfully
+               
+  // situation that tool is used successfully
                else{
                   current.x -= 2;
                   *num_of_tool -= 1;}
             }
-            // the situation of move left
+            
+  // the situation of move left
             else if (input == 'a'){
                if (current.y == 0 || current.y == 1){
                   cout << "You reach the boundary, invalid movement!" << endl;
@@ -105,7 +117,8 @@ int main(){
                   current.y -= 2;
                   *num_of_tool -= 1;}
             }
-            //the situation of move down
+            
+   //the situation of move down
             else if (input == 's'){
                if (current.x == 9 || current.x == 8){
                   cout << "You reach the boundary, invalid movement!" << endl;
@@ -119,7 +132,8 @@ int main(){
                   current.x += 2;
                   *num_of_tool -=1;}
             }
-            // the situation of move right
+            
+ // the situation of move right
             else if (input == 'd'){
                if (current.y == 9 || current.y == 8){
                   cout << "You reach the boundary, invalid movement!" << endl;
@@ -135,7 +149,8 @@ int main(){
             }
          }
       }
-      // the situation that move up without using tool
+      
+// the situation that move up without using tool
       else if (input == 'w'){
         if (current.x == 0){
           cout << "You reach the boundary, invalid movement!" << endl;
@@ -148,7 +163,8 @@ int main(){
         else
           current.x -= 1;
       }
-      // the situation that move left without using tool
+      
+// the situation that move left without using tool
       else if (input == 'a'){
         if (current.y == 0){
           cout << "You reach the boundary, invalid movement!" << endl;
@@ -161,7 +177,8 @@ int main(){
         else
           current.y -= 1;
       }
-       // the situation that move down without using tool
+       
+// the situation that move down without using tool
       else if (input == 's'){
         if (current.x == 9){
           cout << "You reach the boundary, invalid movement!" << endl;
@@ -174,7 +191,8 @@ int main(){
         else
           current.x += 1;
       }
-       // the situation that move right without using tool
+       
+ // the situation that move right without using tool
       else if (input == 'd'){
         if (current.y == 9){
           cout << "You reach the boundary, invalid movement!" << endl;
@@ -187,17 +205,20 @@ int main(){
         else
           current.y += 1;
       }
-      // the situation that you run into a landmine 
+      
+// the situation that you run into a landmine 
       if ( map[current.x][current.y] == 100 ){
          cout << "You reach a landmine!" << endl;
          *blood-=1;
-      // the situation that you run out of your blood and lose the game
+      
+// the situation that you run out of your blood and lose the game
          if (*blood == 0){
             cout << "You use up all your blood" << endl << "You lose!" <<endl;
             cout << "Your final mark is -9999!" <<endl;
             break;}
-      // the situation that run into a landmine but still alive 
-      // player will be sent to a random positon in the first colomn and start their journey again
+      
+// the situation that run into a landmine but still alive 
+// player will be sent to a random positon in the first colomn and start their journey again
          else if (*blood > 0){
             cout << "Go back and try again!" << endl;
             int restart_pos=rand()%10;
@@ -207,11 +228,12 @@ int main(){
             continue;}
         }
       
-      //the situation that player reach destination can win the game
+//the situation that player reach destination can win the game
       else if ( current.x == end.x && current.y == 9){
          cout << "Congratulation, you reach the endpoint!" << endl;
          cout << "You win!" <<endl;
-         // at the end, calculate the mark according to critria set in the instruction
+         
+// at the end, calculate the mark according to critria set in the instruction
          *mark += (*blood)*100;
          if(number_of_heal_pack==0){
          *mark += 200;}
@@ -222,12 +244,14 @@ int main(){
          else if(*num_step>40){
          *mark -= ((*num_step)-40)*8;
          }
-         // return total mark to the player to rate his performance
+         
+// return total mark to the player to rate his performance
          cout << "You have used " << *num_step << " movements to reach the endpoint." <<endl;
          cout<<"Your final mark is: "<<*mark<<endl;
          break;
       }
-      // the situation that player pick up a heal pack
+      
+// the situation that player pick up a heal pack
       else if ( current.x == heal_pack.x && current.y == heal_pack.y){
          if (number_of_heal_pack == 0){
             cout << "You have already picked up the heal pack. You cannot pick the heal pack any more!" << endl;
@@ -245,7 +269,8 @@ int main(){
          continue;
       }
    }
-   // delete all dynamaic menory
+   
+// delete all dynamaic menory
    delete blood;
    blood = 0;
    delete mark;
