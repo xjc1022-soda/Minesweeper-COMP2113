f7038357cb9c6d161e8cb81eb762bc5a81e05777
xjc1022-soda <80735003+xjc1022-soda@users.noreply.github.com>
2021-04-12 13:27:48 +0800

    Update printmap.cpp

--- printmap.cpp
+++ printmap.cpp
@@ -5,11 +5,11 @@ using namespace std;
 // to visialize the map for the player 
 // show exsiting position and heal pack for the player
 void printmap(int map[][10], Position heal_pack, Position current){
-   for(int i=0; i<10; i++){
-    for(int j=0; j<10; j++){
-        if (i==current.x && current.y==j)
+   for(int i = 0; i<10; i++){
+    for(int j = 0; j<10; j++){
+        if (i == current.x && current.y == j)
             cout<<"O"<<" ";
-        else if(i==heal_pack.x && j==heal_pack.y)
+        else if(i == heal_pack.x && j == heal_pack.y)
             cout<<"*"<<" ";
         else
             cout<<"x"<<" ";
